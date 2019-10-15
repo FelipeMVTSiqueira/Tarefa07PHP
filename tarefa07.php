2858a3d21ec64dccfc62e94e8ffe8e5ac598d51d
Felipe Siqueira <felipesiqueira1212@gmail.com>
2019-10-15 20:52:51 -0300

    Resolução Ex 1f)

--- tarefa07.php
+++ tarefa07.php
@@ -1,5 +1,5 @@
 <?php
-    $a=['a'=>1, 'b'=>2, 'c'=>'Eu <3 JSON'];
+    $a=["a"=>1, "b"=>2, "c"=>"Eu <3 JSON"];
     foreach($a as $nome=>$dado){
         echo $nome."=>".$dado." ";
     }
@@ -8,9 +8,13 @@
     echo $a;
     echo "<br>"."<br>";
 
-    $b=json_decode($a);
+    $b=json_decode($a,true);
     foreach($b as $nome=>$dado){
-        echo $nome."=>".$dado." ";
+        echo $nome."=>".$dado."<br>";
     }
+
+    echo $b["c"]." | ".$b["a"]." | ".$b["b"]." | ";
+
+    echo "<br<"."<br>";
     
     
