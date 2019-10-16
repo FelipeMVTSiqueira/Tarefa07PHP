15b6cab832372e2f508cc71d26812292773773e2
Felipe Siqueira <felipesiqueira1212@gmail.com>
2019-10-16 16:01:29 -0300

    Resolução Ex 2) do jeito difícil

--- tarefa07.php
+++ tarefa07.php
@@ -13,10 +13,32 @@
         echo $nome."=>".$dado."<br>";
     }
 
-    echo $b["c"]." | ".$b["a"]." | ".$b["b"]." | ";
-
-    echo "<br<"."<br>";
-    
+    echo $b["c"]." | ".$b["a"]." | ".$b["b"]." | "."<br>"."<br>";
 
     
-    
+$categorias=json_decode(file_get_contents('categorias.json'),true);
+
+var_dump($categorias);
+
+echo "<br>"."<br>";
+
+
+foreach($categorias as $key => $arrays){
+    echo$key."<br>"."<br>";
+    foreach($arrays as $array){
+        foreach($array as $key => $value){
+            if(is_numeric($value)){
+            } else {?>
+                <!DOCTYPE html>
+                    <html>
+                        <input type="checkbox">
+                    </html>
+                <?php echo $value;
+                echo "<br>"."<br>";
+            };
+        }
+    }
+    echo "<br />";
+}
+
+echo "<br>"."<br>";
