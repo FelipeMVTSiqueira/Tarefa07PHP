66ee89ccc99384385ca37e9b0c43e749c6f6c5f9
Felipe Siqueira <felipesiqueira1212@gmail.com>
2019-10-17 15:12:52 -0300

    Resolução Ex 3c)

--- arquivos.php
+++ arquivos.php
@@ -1,14 +1,20 @@
+<!-- <?php
+    // $idArquivo = "texto.txt";
+    // function taAqui($texto){
+    //     for($i=0;$i<50;$i++){
+    //         $abrindoArquivo = fopen($texto, "a+");?>
+    //     <!DOCTYPE html>
+    //         <html>
+    //             <?php //file_put_contents($texto,"Ola, mundo!,testando"."\r\n", FILE_APPEND);?>
+    //         </html>
+    //     <?php
+    //     }
+    // };
+    // taAqui($idArquivo);
+    // $arquivo = fopen('texto.txt', 'w');
+    // $tamanho = filesize('texto.txt');
+    // $conteudo = fread($arquivo, $tamanho);
+?> -->
 <?php
-    $idArquivo = "texto.txt";
-    function taAqui($texto){
-        for($i=0;$i<50;$i++){
-        $abrindoArquivo = fopen($texto, "a+");?>
-        <!DOCTYPE html>
-            <html>
-                <?php file_put_contents($texto,"Ola, mundo!,testando"."\r\n", FILE_APPEND);?>
-            </html>
-        <?php
-        }
-    };
-    taAqui($idArquivo);
-?>
+    $arquivo = file_get_contents("texto.txt");
+    echo $arquivo;
