a01355aa471a1e08a335bc8c5b8665ec531f5e6a
Felipe Siqueira <felipesiqueira1212@gmail.com>
2019-10-17 15:40:30 -0300

    Resolução Ex 3h)

--- arquivos.php
+++ arquivos.php
@@ -1,5 +1,7 @@
 <?php
     $mensagem = "Este texto sobrescreve o anterior?";
+    file_get_contents("texto2.txt");
+    $mensagem .= "NAO!";
     file_put_contents("texto2.txt",$mensagem); 
-    //Este texto sobrescreve o anterior SIM.
+    //Este texto NAO sobrescreve o anterior.
 ?>
