0295927bfd2f3a0725404444d6c5118740eba3e4
Felipe Siqueira <felipesiqueira1212@gmail.com>
2019-10-17 15:35:23 -0300

    Resolução Ex 3g)

--- arquivos.php
+++ arquivos.php
@@ -1,25 +1,5 @@
-<!-- <?php
-    // $idArquivo = "texto.txt";
-    // function taAqui($texto){
-    //     for($i=0;$i<50;$i++){
-    //         $abrindoArquivo = fopen($texto, "a+");?>
-    //     <!DOCTYPE html>
-    //         <html>
-    //             <?php //file_put_contents($texto,"Ola, mundo!,testando"."\r\n", FILE_APPEND);?>
-    //         </html>
-    //     <?php
-    //     }
-    // };
-    // taAqui($idArquivo);
-    // $arquivo = fopen('texto.txt', 'w');
-    // $tamanho = filesize('texto.txt');
-    // $conteudo = fread($arquivo, $tamanho);
-?> -->
 <?php
-    $arquivo = fopen("texto.txt","r");
-    if ($arquivo){
-        while (($linha = fgets($arquivo))) {
-            echo $linha;
-        }
-    }
-    fclose($arquivo);
+    $mensagem = "Este texto sobrescreve o anterior?";
+    file_put_contents("texto2.txt",$mensagem); 
+    //Este texto sobrescreve o anterior SIM.
+?>
